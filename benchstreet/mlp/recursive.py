8693028c3e1a7c4b78da1618ecfdb80c50bb994f
ColonelParrot <65585002+ColonelParrot@users.noreply.github.com>
2025-07-18 15:10:51 -0400

    add mlp recursive and vector

---
+++ benchstreet/mlp/recursive.py
@@ -0,0 +1,55 @@
+import numpy as np
+
+from keras.models import Sequential
+from keras.layers import Dense
+from sklearn.preprocessing import MinMaxScaler
+
+from benchstreet import util
+
+dataset = util.getDataFrame()
+price_series = util.getPrice(dataset)
+
+window_size = 730
+horizon = 1
+epochs = 100
+n_features = 1
+
+# normalize data
+scaler = MinMaxScaler()
+scaled_sequence = scaler.fit_transform(price_series.values.reshape(-1, 1)).flatten()
+# not a multi-step predictor so horizon must be custom set
+train_seq, test_seq, split_idx = util.split_train_test(scaled_sequence, 730)
+
+# split data
+X, y = util.split_sequence(train_seq, window_size, horizon)
+X = X.reshape((X.shape[0], X.shape[1], n_features))
+
+# build model
+model = Sequential()
+model.add(Dense(100, activation='relu', input_dim=window_size))
+model.add(Dense(1))
+model.compile(optimizer='adam', loss='mse')
+
+# fit model
+model.fit(X, y, epochs=epochs, verbose=1)
+
+predictions = []
+
+test_input = train_seq[-window_size:].tolist()
+
+for i in range(len(test_seq)):
+    x_input = np.array(test_input[-window_size:]).reshape((1, window_size, 1))
+    predicted_y = model.predict(x_input, verbose=0)
+    predicted_price = float(predicted_y[0, 0])
+
+    predictions.append(predicted_price)
+    test_input.append(predicted_price)
+
+    print(f"Step {i + 1}/{len(test_seq)} - Prediction {predicted_price}")
+
+predictions = scaler.inverse_transform(np.array(predictions).reshape(-1, 1)).flatten()
+actual = scaler.inverse_transform(test_seq.reshape(-1, 1)).flatten().reshape(1,-1).flatten()
+mae = util.calculateMAE(actual, predictions)
+
+util.graph_comparison('S&P 500 MLP Recursive Prediction', price_series, mae, price_series,
+                      predictions, split_idx)
---
+++ benchstreet/mlp/vector.py
@@ -0,0 +1,47 @@
+from numpy import array
+
+from keras.models import Sequential
+from keras.layers import Dense
+from sklearn.preprocessing import MinMaxScaler
+
+from benchstreet import util
+
+dataset = util.getDataFrame()
+price_series = util.getPrice(dataset)
+
+window_size = 730
+horizon = 730
+epochs = 200
+n_features = 1
+
+# normalize data
+scaler = MinMaxScaler()
+scaled_sequence = scaler.fit_transform(price_series.values.reshape(-1, 1)).flatten()
+train_seq, test_seq, split_idx = util.split_train_test(scaled_sequence, horizon)
+
+# split data
+X, y = util.split_sequence(train_seq, window_size, horizon)
+X = X.reshape((X.shape[0], X.shape[1], n_features))
+
+# build model
+model = Sequential()
+model.add(Dense(100, activation='relu', input_dim=window_size))
+model.add(Dense(horizon))
+model.compile(optimizer='adam', loss='mse')
+
+# fit model
+model.fit(X, y, epochs=epochs, verbose=1)
+
+# predict future prices
+test_input = array(train_seq[-window_size:]).reshape((1, window_size, n_features))
+scaled_predictions = model.predict(test_input, verbose=1)
+predictions = scaler.inverse_transform(scaled_predictions.reshape(-1, 1)).flatten()
+
+print("Predicted (unnormalized) prices:")
+print(predictions)
+
+actual = scaler.inverse_transform(test_seq.reshape(-1, 1))
+mae = util.calculateMAE(actual, predictions)
+
+util.graph_comparison('S&P 500 MLP Multi-Step (Vector) Prediction', price_series, mae, price_series,
+                      predictions, split_idx)
