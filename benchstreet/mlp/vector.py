7ca77633c80ec83eaea25e162717137b800fb7f0
ColonelParrot <65585002+ColonelParrot@users.noreply.github.com>
2025-07-18 15:38:10 -0400

    fix import

--- benchstreet/mlp/vector.py
+++ benchstreet/mlp/vector.py
@@ -1,4 +1,4 @@
-from numpy import array
+import numpy as np
 
 from keras.models import Sequential
 from keras.layers import Dense
@@ -33,7 +33,7 @@ model.compile(optimizer='adam', loss='mse')
 model.fit(X, y, epochs=epochs, verbose=1)
 
 # predict future prices
-test_input = array(train_seq[-window_size:]).reshape((1, window_size, n_features))
+test_input = np.array(train_seq[-window_size:]).reshape((1, window_size, n_features))
 scaled_predictions = model.predict(test_input, verbose=1)
 predictions = scaler.inverse_transform(scaled_predictions.reshape(-1, 1)).flatten()
 
